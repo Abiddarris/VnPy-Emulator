fd5fc73d52373519e58582ef78416a3526892c28
Abiddaris <abiddaris1101@gmail.com>
2024-05-27 09:36:14 +0800

    Dump error message if crash

--- app/src/main/java/com/abiddarris/vnpyemulator/VnPyApplication.java
+++ app/src/main/java/com/abiddarris/vnpyemulator/VnPyApplication.java
@@ -19,12 +19,30 @@ package com.abiddarris.vnpyemulator;
 
 import android.app.Application;
 import com.google.android.material.color.DynamicColors;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class VnPyApplication extends Application {
     
     @Override
     public void onCreate() {
         super.onCreate();
+        
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+            getExternalFilesDir(null).mkdirs();
+            try (var writer = new PrintWriter(new BufferedWriter(
+                new FileWriter(new File(getExternalFilesDir(null), "error.txt"), true)))) {
+                 
+                e.printStackTrace(writer);
+                writer.flush();    
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            System.exit(1);
+        });
         
         DynamicColors.applyToActivitiesIfAvailable(this);
     }
