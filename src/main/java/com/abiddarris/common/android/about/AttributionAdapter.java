242356ba9e143393c62e8e2bd9d6b40a3c615b54
Abiddaris <abiddaris1101@gmail.com>
2024-06-03 08:52:35 +0800

    feat(common): View License button will show full license text

--- src/main/java/com/abiddarris/common/android/about/AttributionAdapter.java
+++ src/main/java/com/abiddarris/common/android/about/AttributionAdapter.java
@@ -23,33 +23,36 @@ import androidx.recyclerview.widget.RecyclerView.Adapter;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import com.abiddarris.common.android.about.AttributionAdapter.AttributionViewHolder;
 import com.abiddarris.common.databinding.LayoutAttributionBinding;
+import java.util.function.BiConsumer;
 
 public class AttributionAdapter extends Adapter<AttributionViewHolder> {
-    
+
     private Context context;
+    private BiConsumer<Integer, Attribution> callback;
     private LayoutInflater inflater;
     private Attribution[] attributions;
-    
+
     public AttributionAdapter(Context context, Attribution[] attributions) {
         this.context = context;
         this.attributions = attributions;
-        
+
         inflater = LayoutInflater.from(context);
     }
-    
+
     @Override
     public AttributionViewHolder onCreateViewHolder(ViewGroup group, int type) {
-        return new AttributionViewHolder(
-            LayoutAttributionBinding.inflate(inflater, group, false));
+        return new AttributionViewHolder(LayoutAttributionBinding.inflate(inflater, group, false));
     }
 
     @Override
     public void onBindViewHolder(AttributionViewHolder holder, int index) {
         Attribution attribution = attributions[index];
-        
+
         holder.binding.header.setText(attribution.getHeader());
         holder.binding.viewLicenseButton.setOnClickListener((view) -> {
-            
+            if(callback != null) {
+                callback.accept(index, attribution);
+            }
         });
     }
 
@@ -58,15 +61,23 @@ public class AttributionAdapter extends Adapter<AttributionViewHolder> {
         return attributions.length;
     }
     
+    public BiConsumer<Integer, Attribution> getCallback() {
+        return this.callback;
+    }
+
+    public void setCallback(BiConsumer<Integer, Attribution> callback) {
+        this.callback = callback;
+    }
+
     public static class AttributionViewHolder extends ViewHolder {
-        
+
         private LayoutAttributionBinding binding;
-        
+
         private AttributionViewHolder(LayoutAttributionBinding binding) {
             super(binding.getRoot());
-            
+
             this.binding = binding;
         }
-        
     }
+
 }
