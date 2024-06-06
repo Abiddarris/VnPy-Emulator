fc15a61a4f151a88296e428e126e39d6b5fb42df
Abiddaris <abiddaris1101@gmail.com>
2024-06-06 17:04:45 +0800

    fix(common): Disable positive button if no item is selected

--- common/src/main/java/com/abiddarris/common/android/dialogs/SingleChoiceDialog.java
+++ common/src/main/java/com/abiddarris/common/android/dialogs/SingleChoiceDialog.java
@@ -16,10 +16,14 @@
 package com.abiddarris.common.android.dialogs;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import com.abiddarris.common.R;
 import com.abiddarris.common.databinding.DialogSingleChoiceBinding;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -27,6 +31,9 @@ import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 import java.util.Optional;
 
 public class SingleChoiceDialog extends BaseDialogFragment<Integer> {
+    
+    private boolean dialogNull;
+    private boolean enablePositiveButton;
     
     private DialogSingleChoiceBinding ui;
     private int selection;
@@ -37,8 +44,19 @@ public class SingleChoiceDialog extends BaseDialogFragment<Integer> {
         return new SingleChoiceDialogBuilder(getContext());
     }
     
+    @Override
+    protected void onDialogCreated(AlertDialog dialog, Bundle savedInstanceState) {
+        super.onDialogCreated(dialog, savedInstanceState);
+        
+        if(dialogNull) {
+            dialog.setOnShowListener(v -> enablePositiveButton(enablePositiveButton));
+        }
+    }
+    
     protected void onSelected(int selection) {
         this.selection = selection;
+        
+        enablePositiveButton(selection >= 0);
     }
     
     public void setItems(String[] items, int selection) {
@@ -51,7 +69,7 @@ public class SingleChoiceDialog extends BaseDialogFragment<Integer> {
         
         String text = selection >= 0 ? items[selection] : getString(R.string.select_item);
         spinner.setText(text, false);
-        
+       
         onSelected(selection);
         
         spinner.setOnItemClickListener((adapterView, view, index, id) -> onSelected(index));
@@ -69,6 +87,23 @@ public class SingleChoiceDialog extends BaseDialogFragment<Integer> {
     public int getSelection() {
     	return selection;
     }
+    
+    private void enablePositiveButton(boolean enabled) {
+        var dialog = (AlertDialog) getDialog();
+        if(dialog == null) {
+            dialogNull = true;
+            enablePositiveButton = enabled;
+            return;
+        }
+        
+        Button button = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        if(button == null) {
+            dialog.setOnShowListener(v -> enablePositiveButton(enabled));
+            return;
+        }
+        button.setEnabled(enabled);
+    }
+    
     
     private class SingleChoiceDialogBuilder extends DialogBuilder {
         
@@ -102,4 +137,5 @@ public class SingleChoiceDialog extends BaseDialogFragment<Integer> {
         }
         
     }
+
 }
