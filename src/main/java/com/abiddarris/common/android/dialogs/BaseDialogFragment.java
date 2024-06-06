3cea7845fb15e35b4ef18657b6fac990f912d18f
Abiddaris <abiddaris1101@gmail.com>
2024-06-06 14:26:29 +0800

    feat(common): Add Result API

--- src/main/java/com/abiddarris/common/android/dialogs/BaseDialogFragment.java
+++ src/main/java/com/abiddarris/common/android/dialogs/BaseDialogFragment.java
@@ -16,12 +16,50 @@
 package com.abiddarris.common.android.dialogs;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.annotation.CallSuper;
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import com.abiddarris.common.utils.ObjectWrapper;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Consumer;
 
-public class BaseDialogFragment extends DialogFragment {
+public class BaseDialogFragment<Result> extends DialogFragment {
+    
+    private static final Handler HANDLER = new Handler(Looper.getMainLooper());
+    private static final String ID = "id";
+    private static final String RESULT_CALLED = "resultCalled";
+    private static final String RESULT_LISTENER = "resultListener";
+    private static final Map<String, Map<String, Object>> NON_SERIALIZABLE_OBJECTS = new HashMap<>();
+    private static final Random RANDOM = new Random();
+    
+    private BaseDialogViewModel model;
+    private Map<String, Object> variables = new HashMap<>();
+    
+    @Override
+    @CallSuper
+    @MainThread
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        model = new ViewModelProvider(this)
+            .get(BaseDialogViewModel.class);
+        variables = model.attach(variables, this);
+    }    
     
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -36,6 +74,77 @@ public class BaseDialogFragment extends DialogFragment {
         onDialogCreated(dialog, savedInstanceState);
         
         return dialog;
+    }
+
+    @Nullable
+    public Result showForResultAndBlock(@NonNull FragmentManager manager) {
+        ObjectWrapper<Result> lock = new ObjectWrapper<>();
+        ObjectWrapper<Boolean> called = new ObjectWrapper<>(false);
+        showForResult(manager, (val) -> {
+            lock.setObject(val);
+            called.setObject(true);
+               
+            synchronized(lock) {
+                lock.notifyAll();
+            }
+        });
+        if(!called.getObject()) {
+            synchronized(lock) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }
+        System.out.println("Returns result for " + getClass() + " with value of : " + lock.getObject());
+        return lock.getObject();
+    }
+    
+    public void showForResult(@NonNull FragmentManager manager, @Nullable Consumer<Result> resultListener) {
+        saveVariable(RESULT_LISTENER, resultListener);
+        
+        show(manager, null);
+    }
+    
+    protected synchronized void sendResult(@Nullable Result result) {
+        System.out.println("send result called for " + getClass() + " with hash " + hashCode()  );
+        System.out.println(NON_SERIALIZABLE_OBJECTS);
+        
+        Consumer<Result> listener = getVariable(RESULT_LISTENER);
+        if(listener == null) {
+            return;
+        }
+        if(this.<Boolean>getVariable(RESULT_CALLED, false)) {
+            throw new IllegalStateException("Cannot call sendResult more than once");
+        }
+        
+        saveVariable(RESULT_CALLED, true);
+        
+        listener.accept(result);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Nullable
+    protected <T> T getVariable(@Nullable String name) {
+        return getVariable(name, null);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Nullable
+    protected <T> T getVariable(@Nullable String name, T defaultVal) {
+        return (T)variables.getOrDefault(name, defaultVal);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Nullable
+    protected <T> T saveVariable(@Nullable String name, @Nullable T obj) {
+        return (T)variables.put(name, obj);
+    }
+    
+    @Nullable
+    protected Result getDefaultResult() {
+        return null;
     }
     
     /**
@@ -53,5 +162,33 @@ public class BaseDialogFragment extends DialogFragment {
     }
     
     protected void onDialogCreated(AlertDialog dialog, Bundle savedInstanceState) {
+    }
+    
+    public static class BaseDialogViewModel extends ViewModel {
+        
+        private Map<String, Object> variables = null;
+        private BaseDialogFragment fragment;
+        
+        private Map<String, Object> attach(Map<String, Object> variables, BaseDialogFragment fragment) {
+            this.fragment = fragment;
+            
+            if(this.variables == null) {
+                this.variables = variables;
+            }
+            return this.variables;
+        }
+        
+        @Override
+        @SuppressWarnings("unchecked")
+        protected void onCleared() {
+            super.onCleared();
+            
+            try {
+                fragment.sendResult(fragment.getDefaultResult());
+            } catch (IllegalStateException ignored) {
+                ignored.printStackTrace();
+            }
+        }
+        
     }
 }
