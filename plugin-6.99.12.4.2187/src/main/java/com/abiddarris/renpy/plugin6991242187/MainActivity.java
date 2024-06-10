dce452e088d5060100dbdbb9e32c7aa1c8143f20
Abiddaris <abiddaris1101@gmail.com>
2024-06-10 20:37:09 +0800

    fix(plugin-6.99.12.4.2187): Add `MainActivity`

---
+++ plugin-6.99.12.4.2187/src/main/java/com/abiddarris/renpy/plugin6991242187/MainActivity.java
@@ -0,0 +1,12 @@
+package com.abiddarris.renpy.plugin6991242187;
+
+import android.os.Bundle;
+import com.abiddarris.plugin.PermissionActivity;
+
+public class MainActivity extends PermissionActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
