e19212c8bdd29ca60f3969c20c548936faccf70e
Abiddaris <abiddaris1101@gmail.com>
2024-06-10 08:20:28 +0800

    build(app): Move signing key to root project

--- common/src/main/java/com/abiddarris/common/android/dialogs/EditTextDialog.java
+++ common/src/main/java/com/abiddarris/common/android/dialogs/EditTextDialog.java
@@ -21,7 +21,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import androidx.appcompat.app.AlertDialog;
 import com.abiddarris.common.databinding.DialogEditTextBinding;
-import com.abiddarris.common.validator.Validator;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import java.util.Optional;
 
