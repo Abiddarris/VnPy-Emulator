5713dd14aa1d1c7b9648dc88eb8666d047c3cce5
Abiddaris <abiddaris1101@gmail.com>
2024-06-10 20:51:58 +0800

    fix(plugin-6.99.12.4.2187): Apply DynamicColor to `PermissionActivity`

--- plugin-6.99.12.4.2187/src/main/java/com/abiddarris/renpy/plugin6991242187/MainActivity.java
+++ plugin-6.99.12.4.2187/src/main/java/com/abiddarris/renpy/plugin6991242187/MainActivity.java
@@ -2,11 +2,14 @@ package com.abiddarris.renpy.plugin6991242187;
 
 import android.os.Bundle;
 import com.abiddarris.plugin.PermissionActivity;
+import com.google.android.material.color.DynamicColors;
 
 public class MainActivity extends PermissionActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        DynamicColors.applyToActivityIfAvailable(this);
+        
         super.onCreate(savedInstanceState);
     }
 }
