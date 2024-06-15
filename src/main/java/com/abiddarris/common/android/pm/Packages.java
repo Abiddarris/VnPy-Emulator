8e4e1b0837fb5c323498c53614d70de4cd92b0d8
Abiddaris <abiddaris1101@gmail.com>
2024-06-15 10:38:38 +0800

    feat(common): Add Installation API

--- src/main/java/com/abiddarris/common/android/pm/Packages.java
+++ src/main/java/com/abiddarris/common/android/pm/Packages.java
@@ -15,12 +15,29 @@
  ***********************************************************************************/
 package com.abiddarris.common.android.pm;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInstaller;
+import android.content.pm.PackageInstaller.Session;
+import android.content.pm.PackageInstaller.SessionParams;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager.PackageInfoFlags;
+import android.os.Handler;
+import android.os.Looper;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Packages {
+    
+    private static final String ACTION_INSTALLED = "com.abiddarris.common.android.ACTION_INSTALLED";
     
     public static boolean isInstalled(Context context, String packageName) {
         PackageManager manager = context.getPackageManager();
@@ -32,4 +49,51 @@ public class Packages {
         return true;
     }
     
+    public static void installPackage(Context context, File file) throws IOException {
+    	try (InputStream stream = new BufferedInputStream(new FileInputStream(file))){
+            installPackage(context, stream);
+        } 
+    }
+    
+    public static void installPackage(Context context, InputStream stream) throws IOException {
+        PackageInstaller packageInstaller = context.getPackageManager()
+            .getPackageInstaller();
+        SessionParams params = new SessionParams(SessionParams.MODE_FULL_INSTALL);
+       
+        int sessionId = packageInstaller.createSession(params);
+        
+        Session session = packageInstaller.openSession(sessionId);
+        try (OutputStream output = new BufferedOutputStream(session.openWrite("PackageInstaller", 0, -1))) {
+            byte[] buffer = new byte[65536];
+            int length;
+            while ((length = stream.read(buffer)) != -1) {
+                output.write(buffer, 0, length);
+            }
+            output.flush();
+        }
+        
+        InstallationListener listener = new InstallationListener();
+        try {
+            context.registerReceiver(listener, new IntentFilter(ACTION_INSTALLED));
+
+            Intent intent = new Intent(ACTION_INSTALLED);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+            
+            session.commit(pendingIntent.getIntentSender());
+            
+            synchronized(listener) {
+                if(!listener.isReceived()) {
+                    try {
+                        listener.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } finally {
+            context.unregisterReceiver(listener);
+        }
+        
+    }
+
 }
