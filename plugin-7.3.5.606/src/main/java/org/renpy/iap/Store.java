d5fa457480c2dea264598c8b43d25a0f7fb01ba0
Abiddaris <abiddaris1101@gmail.com>
2024-06-20 08:13:08 +0800

    build(plugin-7.3.5.606): Add `Store`

---
+++ plugin-7.3.5.606/src/main/java/org/renpy/iap/Store.java
@@ -0,0 +1,65 @@
+package org.renpy.iap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+public class Store {
+
+    static public Store store = new Store();;
+
+    static public Store getStore() {
+        return store;
+    }
+
+    public void destroy() {
+    }
+
+    public boolean onActivityResult(int requestCode, int resultCode, Intent intent) {
+        return false;
+    }
+
+    public boolean getFinished() {
+        return true;
+    }
+
+    public String getStoreName() {
+        return "none";
+    }
+
+    public ArrayList<String> skus = new ArrayList<String>();
+    public HashMap<String, String> prices = new HashMap<String, String>();
+
+    public void clearSKUs() {
+        skus.clear();
+    }
+
+    public void addSKU(String sku) {
+        skus.add(sku);
+    }
+
+    public void updatePrices() {
+        return;
+    }
+
+    public String getPrice(String sku) {
+        return prices.get(sku);
+    }
+
+    HashSet<String> purchased = new HashSet<String>();
+
+    public boolean hasPurchased(String sku) {
+       return purchased.contains(sku);
+    }
+
+    public void restorePurchases() {
+    }
+
+    public void beginPurchase(String sku) {
+    }
+
+}
