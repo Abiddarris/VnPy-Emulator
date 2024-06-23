c48019706a4dfe238bb8b2d85d67f4ec82b20557
Abiddaris <abiddaris1101@gmail.com>
2024-06-23 15:43:21 +0800

    fix(common): Fix crash if `keycode` is `null`

--- src/main/java/com/abiddarris/common/android/virtualkeyboard/TouchListener.java
+++ src/main/java/com/abiddarris/common/android/virtualkeyboard/TouchListener.java
@@ -16,13 +16,18 @@ class TouchListener implements OnTouchListener {
     
     @Override
     public boolean onTouch(View view, MotionEvent event) {
+        Keycode code = key.getKeycode();
+        if(code == null) {
+            return true;
+        }
+        
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN :
-                keyboard.sendKeyEvent(Event.DOWN, key.getKeycode().getKeycode());
+                keyboard.sendKeyEvent(Event.DOWN, code.getKeycode());
                 return true;    
             case MotionEvent.ACTION_UP :
             case MotionEvent.ACTION_CANCEL :
-                keyboard.sendKeyEvent(Event.UP, key.getKeycode().getKeycode());
+                keyboard.sendKeyEvent(Event.UP, code.getKeycode());
                 return true;
             default :
                 return false;        
