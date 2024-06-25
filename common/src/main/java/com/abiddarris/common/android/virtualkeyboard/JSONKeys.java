080d4ce4f92f7379e86962724e23ad40a3cae44e
Abiddaris <abiddaris1101@gmail.com>
2024-06-25 21:40:58 +0800

    feat(common): Add class containing JSON keys

---
+++ common/src/main/java/com/abiddarris/common/android/virtualkeyboard/JSONKeys.java
@@ -0,0 +1,31 @@
+/***********************************************************************************
+ * Copyright 2024 Abiddarris
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***********************************************************************************/
+
+package com.abiddarris.common.android.virtualkeyboard;
+
+final class JSONKeys {
+    public static final String ALIGNMENT = "alignment";
+    public static final String FLAGS = "flags";
+    public static final String HEIGHT = "height";
+    public static final String KEYCODE = "keycode";
+    public static final String KEYS = "keys";
+    public static final String MARGIN_X = "marginX";
+    public static final String MARGIN_Y = "marginY";
+    public static final String NAME = "name";
+    public static final String SIZE = "size";
+    public static final String TYPE = "type";
+    public static final String WIDTH = "width";
+}
