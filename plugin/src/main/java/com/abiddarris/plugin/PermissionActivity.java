bd7072c7c9c11664e0feca4e5ef73beaee87693d
Abiddaris <abiddaris1101@gmail.com>
2024-06-28 15:45:10 +0800

    feat(plugin): Setup error sender

--- plugin/src/main/java/com/abiddarris/plugin/PermissionActivity.java
+++ plugin/src/main/java/com/abiddarris/plugin/PermissionActivity.java
@@ -1,15 +1,23 @@
 package com.abiddarris.plugin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.abiddarris.common.android.utils.Permissions;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PermissionActivity extends AppCompatActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        setupErrorHandler();
         
         if(Permissions.isManageExternalStorageGranted(this)) {
             permissionGranted();
@@ -21,6 +29,36 @@ public class PermissionActivity extends AppCompatActivity {
                     permissionGranted();
                 }
             });
+    }
+    
+    protected void setupErrorHandler() {
+        Intent intent = getIntent();
+        int port = new PluginArguments(intent)
+                    .getErrorPort();
+        
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            sendError(port, throwable);
+        });
+    }
+    
+    private void sendError(int port, Throwable throwable) {
+        ErrorSenderRunnable runnable = new ErrorSenderRunnable(port, throwable);
+        
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(runnable);
+        
+        synchronized(runnable) {
+            if(!runnable.isDone()) {
+                try {
+                    runnable.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        executor.shutdown();
+        System.exit(1);
     }
     
     protected void permissionGranted() {
