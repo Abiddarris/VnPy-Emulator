4f48666394b23cf08ed99a52ab6c3e6992546a2a
Abiddaris <abiddaris1101@gmail.com>
2024-06-28 17:18:38 +0800

    refactor(app): Move error handling to its own class

--- app/src/main/java/com/abiddarris/vnpyemulator/MainActivity.java
+++ app/src/main/java/com/abiddarris/vnpyemulator/MainActivity.java
@@ -19,28 +19,18 @@ package com.abiddarris.vnpyemulator;
 
 import static com.abiddarris.common.logs.Level.DEBUG;
 
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.abiddarris.common.android.about.AboutActivity;
-import com.abiddarris.common.android.dialogs.ExceptionDialog;
 import com.abiddarris.common.android.tasks.TaskViewModel;
-import com.abiddarris.common.android.utils.Permissions;
 import com.abiddarris.common.logs.Logger;
 import com.abiddarris.common.logs.Logs;
 import com.abiddarris.plugin.PermissionActivity;
@@ -49,22 +39,15 @@ import com.abiddarris.vnpyemulator.databinding.ActivityMainBinding;
 import com.abiddarris.vnpyemulator.dialogs.AboutGameInformationDialog;
 import com.abiddarris.vnpyemulator.dialogs.AddNewGameDialog;
 import com.abiddarris.vnpyemulator.dialogs.DeleteGameDialog;
-import com.abiddarris.vnpyemulator.errors.ErrorHandlerService;
-import com.abiddarris.vnpyemulator.errors.ErrorHandlerService.ErrorHandlerBinder;
-import com.abiddarris.vnpyemulator.errors.OnErrorOccurs;
+import com.abiddarris.vnpyemulator.errors.ErrorViewModel;
 import com.abiddarris.vnpyemulator.games.Game;
 import com.abiddarris.vnpyemulator.patches.PatchRunnable;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class MainActivity extends PermissionActivity implements ServiceConnection, OnErrorOccurs {
+public class MainActivity extends PermissionActivity {
    
     private ActivityMainBinding binding;
-    private ExceptionDialog errorDialog;
-    private ErrorHandlerService service;
+    private ErrorViewModel errorViewModel;
     private Logger log = Logs.newLogger(DEBUG, this);
     private TaskViewModel model;
     private GameAdapter adapter;
@@ -74,15 +57,11 @@ public class MainActivity extends PermissionActivity implements ServiceConnectio
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        if(!bindService(
-            new Intent(this, ErrorHandlerService.class),
-            this, BIND_AUTO_CREATE)) {
-           Toast.makeText(this, "Cannot start ErrorHandlerService", Toast
-                .LENGTH_LONG)
-                .show();
-        }
-        
         model = TaskViewModel.getInstance(this);
+       
+        errorViewModel = new ViewModelProvider(this)
+            .get(ErrorViewModel.class);
+        errorViewModel.attach(this);
         
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         
@@ -98,16 +77,6 @@ public class MainActivity extends PermissionActivity implements ServiceConnectio
     @Override
     protected void setupErrorHandler() {
         //Do nothing
-    }
-    
-    @Override
-    protected void onStart() {
-        super.onStart();
-        
-        if(errorDialog != null) {
-            errorDialog.show(getSupportFragmentManager(), null);
-            errorDialog = null;
-        }
     }
     
     @Override
@@ -166,33 +135,9 @@ public class MainActivity extends PermissionActivity implements ServiceConnectio
         
         return false;
     }
-    
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        
-        unbindService(this);
-    }
-    
-    @Override
-    public void onServiceConnected(ComponentName name, IBinder binder) {
-        service = ((ErrorHandlerBinder)binder)
-            .getService();
-        service.setOnErrorOccurs(this);
-    }
-    
-    @Override
-    public void onServiceDisconnected(ComponentName name) {
-    }
-    
-    @Override
-    public void onErrorOccurs(String applicationName, Throwable throwable) {
-        errorDialog = new ExceptionDialog();
-        errorDialog.setThrowable(throwable);
-    }
-    
+  
     public int getPort() {
-        return service.getPort();
+        return errorViewModel.getPort();
     }
     
     public void open(Game game) {
---
+++ app/src/main/java/com/abiddarris/vnpyemulator/errors/ErrorViewModel.java
@@ -0,0 +1,120 @@
+/***********************************************************************************
+ * Copyright (C) 2024 Abiddarris
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************************/
+package com.abiddarris.vnpyemulator.errors;
+
+import static android.content.Context.BIND_AUTO_CREATE;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+
+import androidx.lifecycle.GenericLifecycleObserver;
+import androidx.lifecycle.Lifecycle.Event;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModel;
+
+import com.abiddarris.common.android.dialogs.ExceptionDialog;
+import com.abiddarris.common.logs.Level;
+import com.abiddarris.common.logs.Logger;
+import com.abiddarris.common.logs.Logs;
+import com.abiddarris.vnpyemulator.MainActivity;
+import com.abiddarris.vnpyemulator.errors.ErrorHandlerService.ErrorHandlerBinder;
+
+public class ErrorViewModel extends ViewModel 
+        implements ServiceConnection, OnErrorOccurs, LifecycleEventObserver {
+   
+    private boolean show;
+    private Logger debug = Logs.newLogger(Level.DEBUG, this);
+    private ExceptionDialog errorDialog;
+    private ErrorHandlerService service;
+    private MainActivity activity;
+    
+    public void attach(MainActivity activity) {
+        boolean firstTime = this.activity == null;
+        
+        this.activity = activity;
+        activity.getLifecycle()
+            .addObserver(this);
+        
+        if(!firstTime)
+            return;
+        
+        activity.bindService(
+            new Intent(activity, ErrorHandlerService.class),
+            this, BIND_AUTO_CREATE
+        );
+    }
+    
+    public int getPort() {
+        return service.getPort();
+    }
+    
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder binder) {
+        service = ((ErrorHandlerBinder)binder)
+            .getService();
+        service.setOnErrorOccurs(this);
+    }
+    
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+    }
+    
+    @Override
+    public void onErrorOccurs(String applicationName, Throwable throwable) {
+        errorDialog = new ExceptionDialog();
+        errorDialog.setThrowable(throwable);
+        
+        showDialog();
+    }
+    
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        
+        activity.unbindService(this);
+    }
+    
+    @Override
+    public void onStateChanged(LifecycleOwner owner, Event event) {
+        debug.log("State changes for " + owner + " to " + event.name());
+        switch(event) {
+            case ON_CREATE :
+            case ON_START :
+            case ON_RESUME :
+                show = true;
+                showDialog();
+                break;
+            case ON_PAUSE :
+            case ON_STOP :
+            case ON_DESTROY :
+                show = false;
+        }
+    }
+    
+    private void showDialog() {
+        if(!show || errorDialog == null) {
+            return;
+        }
+        
+        errorDialog.show(activity.getSupportFragmentManager(), null);
+        errorDialog = null;
+    }
+}
