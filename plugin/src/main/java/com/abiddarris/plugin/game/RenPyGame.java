83919466e80d80a73b16fb43ae06072502646aac
Abiddaris <abiddaris1101@gmail.com>
2024-06-30 19:52:07 +0800

    feat(common): Show notification

--- plugin/src/main/java/com/abiddarris/plugin/game/RenPyGame.java
+++ plugin/src/main/java/com/abiddarris/plugin/game/RenPyGame.java
@@ -17,16 +17,21 @@
  ***********************************************************************************/
 package com.abiddarris.plugin.game;
 
+import android.content.pm.ApplicationInfo;
 import static android.widget.FrameLayout.LayoutParams.MATCH_PARENT;
 
 import static androidx.core.app.NotificationManagerCompat.IMPORTANCE_DEFAULT;
 
+import android.app.Notification;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 
 import com.abiddarris.common.android.virtualkeyboard.VirtualKeyboard;
 import com.abiddarris.common.android.virtualkeyboard.VirtualKeyboardOptions;
@@ -36,14 +41,19 @@ import com.abiddarris.plugin.R;
 import org.libsdl.app.SDLActivity;
 import org.renpy.android.PythonSDLActivity;
 
-public class RenPyGame {
+public class RenPyGame implements DefaultLifecycleObserver {
     
     public static final String GAME_OVERLAY_ID = "game_overlay";
+  
+    private static final int NOTIFICATION_ID = 7890;
     
     private static RenPyGame game;
     
     private PluginArguments arguments;
     private PythonSDLActivity activity;
+    
+    private Notification notification;
+    private NotificationManagerCompat notificationManager;
     
     private RenPyGame(PythonSDLActivity activity) {
         this.activity = activity;
@@ -58,14 +68,41 @@ public class RenPyGame {
     }
     
     public void onCreate(Bundle savedInstanceState) {
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(activity);
+        notificationManager = NotificationManagerCompat.from(activity);
         notificationManager.createNotificationChannel(
             new NotificationChannelCompat.Builder(GAME_OVERLAY_ID, IMPORTANCE_DEFAULT)
                 .setName(activity.getString(R.string.game_overlay_channel_title))
                 .setDescription(activity.getString(R.string.game_overlay_channel_desc))
                 .build()
         );
+        
+        activity.getLifecycle()
+            .addObserver(this);
+        
+        ApplicationInfo info = activity.getApplicationInfo();
+        
+        notification = new NotificationCompat.Builder(activity, GAME_OVERLAY_ID)
+            .setOngoing(true)
+            .setShowWhen(false)
+            .setSmallIcon(info.icon)
+            .setContentTitle(activity.getString(info.labelRes))
+            .setContentText(activity.getString(R.string.notification_text))
+            .build();
     }    
+    
+    @Override
+    public void onResume(LifecycleOwner owner) {
+        DefaultLifecycleObserver.super.onResume(owner);
+        
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+    
+    @Override
+    public void onPause(LifecycleOwner owner) {
+        DefaultLifecycleObserver.super.onPause(owner);
+        
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
     
     public void setContentView(View view) {
         var keyboard = new VirtualKeyboard(activity);
