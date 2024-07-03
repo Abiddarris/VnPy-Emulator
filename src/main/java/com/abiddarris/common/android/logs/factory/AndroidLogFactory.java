1ad8371ae94c5ac988997409cfebcb5be05ce30f
Abiddaris <abiddaris1101@gmail.com>
2024-07-03 19:03:56 +0800

    feat(common): Add `Logger` implementation to log to Android `Log`

---
+++ src/main/java/com/abiddarris/common/android/logs/factory/AndroidLogFactory.java
@@ -0,0 +1,30 @@
+/***********************************************************************************
+ * Copyright 2024 Abiddarris
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***********************************************************************************/
+package com.abiddarris.common.android.logs.factory;
+
+import com.abiddarris.common.android.logs.AndroidLogger;
+import com.abiddarris.common.logs.Level;
+import com.abiddarris.common.logs.Logger;
+import com.abiddarris.common.logs.factory.LogFactory;
+
+public class AndroidLogFactory implements LogFactory {
+    
+    @Override
+    public Logger newLogger(Level level, String tag) {
+        return new AndroidLogger(level, tag);
+    }
+    
+}
