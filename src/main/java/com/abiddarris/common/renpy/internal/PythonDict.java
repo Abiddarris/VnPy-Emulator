d0afc30ecab2ff6f0f2cc985ed0e32f8a8b45933
Abiddaris <abiddaris1101@gmail.com>
2024-08-16 10:07:06 +0800

    refactor(common): Move `PythonDict` to its own class

---
+++ src/main/java/com/abiddarris/common/renpy/internal/PythonDict.java
@@ -0,0 +1,76 @@
+/***********************************************************************************
+ * Copyright 2024 Abiddarris
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***********************************************************************************/
+package com.abiddarris.common.renpy.internal;
+
+import static com.abiddarris.common.renpy.internal.PythonObject.*;
+
+import com.abiddarris.common.renpy.internal.signature.PythonArgument;
+
+import java.util.Iterator;
+import java.util.Map;
+
+class PythonDict extends PythonObject {
+    
+    private static PythonObject dict_iterator;
+        
+    static void init() {
+        dict_iterator = type.callAttribute("__new__", new PythonArgument()
+            .addPositionalArgument(type)
+            .addPositionalArgument(newString("dict_iterator"))
+            .addPositionalArgument(newTuple(object))
+            .addPositionalArgument(newDict()));
+        dict_iterator.setAttribute("__next__", newFunction(
+            PythonObject.findMethod(DictIterator.class, "next"), "self"
+        ));
+    }
+        
+    private Map<PythonObject, PythonObject> map;
+    
+    PythonDict(Map<PythonObject, PythonObject> map) {
+        this.map = map;
+    }
+        
+    private static PythonObject iter(PythonDict self) {
+        return new DictIterator(self.map.keySet().iterator());
+    }
+        
+    private static PythonObject dictGetItem(PythonDict self, PythonObject key) {
+        return self.map.get(key);
+    }
+        
+    private static class DictIterator extends PythonObject {
+            
+        private Iterator<PythonObject> iterator;
+            
+        private DictIterator(Iterator<PythonObject> iterator) {
+            this.iterator = iterator;
+                
+            setAttribute("__class__", dict_iterator);
+        }
+            
+        private static PythonObject next(DictIterator self) {
+            if(self.iterator.hasNext()) {
+                return self.iterator.next();
+            }
+                
+            StopIteration.callAttribute("__new__", new PythonArgument()
+                .addPositionalArgument(StopIteration))
+                .raise();
+            return null;
+        }
+            
+    }
+}
