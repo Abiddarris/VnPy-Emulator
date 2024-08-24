77bddc7dd1d89d5843e4ced206ad798d915903d8
Abiddaris <abiddaris1101@gmail.com>
2024-08-24 20:32:40 +0800

    feat(common): Add `Rpy`

---
+++ src/main/java/com/abiddarris/common/renpy/rpy/Rpy.java
@@ -0,0 +1,12 @@
+
+package com.abiddarris.common.renpy.rpy;
+
+import com.abiddarris.common.renpy.rpy.decompiler.Decompiler;
+
+public class Rpy {
+    
+    public static void initLoader() {
+        Decompiler.initLoader();
+    }
+    
+}
