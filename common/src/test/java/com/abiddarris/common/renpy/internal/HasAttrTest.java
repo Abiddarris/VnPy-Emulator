5ea5d0c2cf7a0402dfad5bf91429583329d52229
Abiddaris <abiddaris1101@gmail.com>
2024-08-27 20:05:52 +0800

    test(common): Add test for `hasattr` on object that has attribute

---
+++ common/src/test/java/com/abiddarris/common/renpy/internal/HasAttrTest.java
@@ -0,0 +1,36 @@
+/***********************************************************************************
+ * Copyright 2024 Abiddarris
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***********************************************************************************/
+package com.abiddarris.common.renpy.internal;
+
+import static com.abiddarris.common.renpy.internal.Python.newString;
+import static com.abiddarris.common.renpy.internal.PythonObject.hasattr;
+import static com.abiddarris.common.renpy.internal.PythonObject.object;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class HasAttrTest {
+    
+    @Test
+    public void existAttributeTest() {
+        PythonObject obj = object.call();
+        obj.setAttribute("name", newString("My Lovely Object"));
+        
+        assertTrue(hasattr.call(obj, newString("name")).toBoolean());
+    }
+    
+}
