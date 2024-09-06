bec7ff24c96fd2c8f812f7c9ba98387ad3846272
Abiddaris <abiddaris1101@gmail.com>
2024-09-06 16:56:33 +0800

    feat(common): Add `__mro__` for `type`

--- common/src/main/java/com/abiddarris/common/renpy/internal/PythonObject.java
+++ common/src/main/java/com/abiddarris/common/renpy/internal/PythonObject.java
@@ -213,6 +213,7 @@ public class PythonObject extends Python implements Iterable<PythonObject> {
         type.setAttribute("__subclasscheck__", newFunction(findMethod(BuiltinsImpl.class, "typeSubclassCheck"), "self", "other"));
         type.setAttribute("__str__", newFunction(findMethod(BuiltinsImpl.class, "typeStr"), "self"));
         type.setAttribute("__instancecheck__", newFunction(BuiltinsImpl.class, "objectInstanceCheck", "self", "other"));
+        type.setAttribute("__mro__", newTuple(type, object));
         
         method = Bootstrap.newClass(type, newTuple(newPythonString("method"), newTuple(object)));
        
