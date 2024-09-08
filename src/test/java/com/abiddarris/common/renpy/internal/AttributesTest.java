5039f46c6c048787de18e53067fcaaaf4de4d2db
Abiddaris <abiddaris1101@gmail.com>
2024-09-08 21:09:02 +0800

    test(common): Add test for \`callNestedAttribute\`

--- src/test/java/com/abiddarris/common/renpy/internal/AttributesTest.java
+++ src/test/java/com/abiddarris/common/renpy/internal/AttributesTest.java
@@ -15,8 +15,13 @@
  ***********************************************************************************/
 package com.abiddarris.common.renpy.internal;
 
+import static com.abiddarris.common.renpy.internal.Python.newFunction;
 import static com.abiddarris.common.renpy.internal.Python.newInt;
+import static com.abiddarris.common.renpy.internal.Python.newString;
+import static com.abiddarris.common.renpy.internal.PythonObject.False;
+import static com.abiddarris.common.renpy.internal.PythonObject.True;
 import static com.abiddarris.common.renpy.internal.PythonObject.object;
+import static com.abiddarris.common.renpy.internal.core.Attributes.callNestedAttribute;
 import static com.abiddarris.common.renpy.internal.core.Attributes.getNestedAttribute;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,6 +39,27 @@ public class AttributesTest {
         root.setAttribute("middle_section", middle);
 
         assertEquals(newInt(3), getNestedAttribute(root, "middle_section.secret"));
+    }
+
+    @Test
+    public void callNestedAttributeTest() {
+        PythonObject middle = object.call();
+        middle.setAttribute("change_value", newFunction(AttributesTest.class, "changeValue", "target"));
+
+        PythonObject root = object.call();
+        root.setAttribute("middle_section", middle);
+
+        PythonObject target = object.call();
+        target.setAttribute("val", False);
+
+        assertEquals(newString("I'm called"), callNestedAttribute(root, "middle_section.change_value", target));
+        assertEquals(True, target.getAttribute("val"));
+    }
+
+    private static PythonObject changeValue(PythonObject target) {
+        target.setAttribute("val", True);
+
+        return newString("I'm called");
     }
 
 }
