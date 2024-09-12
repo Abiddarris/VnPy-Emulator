482f56036eb686ac7aeab7b1246cdd5850f1259e
Abiddaris <abiddaris1101@gmail.com>
2024-09-12 20:07:14 +0800

    feat(common): Add \`PythonObject.subtract\`

--- src/main/java/com/abiddarris/common/renpy/internal/PythonObject.java
+++ src/main/java/com/abiddarris/common/renpy/internal/PythonObject.java
@@ -468,6 +468,10 @@ public class PythonObject extends Python implements Iterable<PythonObject> {
         return callTypeAttribute("__add__", value);
     }
 
+    public PythonObject subtract(PythonObject value) {
+        return callTypeAttribute("__sub__", value);
+    }
+
     public PythonObject getSuper() {
         return super0.call(
             com.abiddarris.common.renpy.internal.core.Types.type(this),
