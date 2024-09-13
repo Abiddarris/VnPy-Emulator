6f88b67419d80e04f65c8ad023d97daec4468d2d
Abiddaris <abiddaris1101@gmail.com>
2024-09-13 17:35:09 +0800

    refactor(common) : Refactoring try statement

---
+++ common/src/main/java/com/abiddarris/common/renpy/internal/trycatch/TryStatement.java
@@ -0,0 +1,58 @@
+/***********************************************************************************
+ * Copyright 2024 Abiddarris
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***********************************************************************************/
+package com.abiddarris.common.renpy.internal.trycatch;
+
+import com.abiddarris.common.renpy.internal.PythonException;
+import com.abiddarris.common.renpy.internal.PythonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class TryStatement {
+
+    private Runnable tryStatement;
+    private List<Except> exceptionStatements = new ArrayList<>();
+    private Runnable finallyStatement;
+    private Runnable elseStatement;
+
+    TryStatement(Runnable tryStatement) {
+        this.tryStatement = tryStatement;
+    }
+
+    void addExceptStatement(ExceptionHandler handler, PythonObject... exceptionsType) {
+        exceptionStatements.add(new Except(handler, exceptionsType));
+    }
+
+    void execute() {
+        try {
+            tryStatement.run();
+        } catch(PythonException e) {
+            PythonObject exception = e.getException();
+            PythonObject exceptionClass = exception.getAttribute("__class__");
+            exceptionStatements.forEach(except -> {
+                if(!Stream.of(except.getExceptions())
+                        .anyMatch(except0 -> except0 == exceptionClass)) {
+                    throw e;
+                }
+
+                except.getExceptionHandler()
+                        .accept(exception);
+            });
+        }
+
+    }
+}
