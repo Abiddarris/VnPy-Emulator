ce6bda2d87a1bd5b3b000e303d6e6e36c3d8aa0a
Abiddaris <abiddaris1101@gmail.com>
2024-09-16 10:08:11 +0800

    test(common): Add test for `Functions.all` that has false element

--- src/test/java/com/abiddarris/common/renpy/internal/core/FunctionsTest.java
+++ src/test/java/com/abiddarris/common/renpy/internal/core/FunctionsTest.java
@@ -3,9 +3,9 @@ package com.abiddarris.common.renpy.internal.core;
 import static com.abiddarris.common.renpy.internal.Python.newInt;
 import static com.abiddarris.common.renpy.internal.Python.newString;
 import static com.abiddarris.common.renpy.internal.Python.newTuple;
+import static com.abiddarris.common.renpy.internal.PythonObject.False;
 import static com.abiddarris.common.renpy.internal.PythonObject.True;
 import static com.abiddarris.common.renpy.internal.core.Functions.all;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -15,5 +15,10 @@ class FunctionsTest {
     @Test
     void all_true() {
         assertEquals(True, all(newTuple(True, newInt(1), newString("something"))));
+    }
+
+    @Test
+    void all_false() {
+        assertEquals(False, all(newTuple(True, newInt(0), newString("something"))));
     }
 }
