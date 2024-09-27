f169124f7a6f4ef0a9f38e1ce449496f68866926
Abiddaris <abiddaris1101@gmail.com>
2024-09-27 22:00:23 +0800

    feat(common): Add `P2Function`

---
+++ src/main/java/com/abiddarris/common/renpy/internal/invocator/P2Invocator.java
@@ -0,0 +1,35 @@
+/***********************************************************************************
+ * Copyright 2024 Abiddarris
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***********************************************************************************/
+package com.abiddarris.common.renpy.internal.invocator;
+
+import com.abiddarris.common.renpy.internal.PythonObject;
+import com.abiddarris.common.renpy.internal.core.functions.P2Function;
+import com.abiddarris.common.renpy.internal.signature.PythonSignature;
+
+public class P2Invocator implements Invocator {
+
+    @Override
+    public PythonObject invoke(Object target, PythonObject[] args) {
+        return ((P2Function)target).execute(args[0], args[1]);
+    }
+
+    @Override
+    public void validateTarget(Object target, PythonSignature signature) {
+        if (!(target instanceof P2Function)) {
+            throwInvalidTargetException(target);
+        }
+    }
+}
