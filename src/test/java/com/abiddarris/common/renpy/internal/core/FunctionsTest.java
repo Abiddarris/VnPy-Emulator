d8da196e7d937d3f9ff6d194b420f687fb149887
Abiddaris <abiddaris1101@gmail.com>
2024-09-29 20:43:03 +0800

    test(common): Add test `max`

--- src/test/java/com/abiddarris/common/renpy/internal/core/FunctionsTest.java
+++ src/test/java/com/abiddarris/common/renpy/internal/core/FunctionsTest.java
@@ -6,6 +6,7 @@ import static com.abiddarris.common.renpy.internal.Python.newTuple;
 import static com.abiddarris.common.renpy.internal.Builtins.False;
 import static com.abiddarris.common.renpy.internal.Builtins.True;
 import static com.abiddarris.common.renpy.internal.core.Functions.all;
+import static com.abiddarris.common.renpy.internal.core.Functions.max;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,10 @@ class FunctionsTest {
     @Test
     void all_false() {
         assertEquals(False, all(newTuple(True, newInt(0), newString("something"))));
+    }
+
+    @Test
+    void max_test() {
+        assertEquals(newInt(4), max(newInt(3), newInt(4)));
     }
 }
