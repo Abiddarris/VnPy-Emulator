671e75fa71e545219bfe30cb594470811084fa22
Abiddaris <abiddaris1101@gmail.com>
2024-11-10 14:14:25 +0800

    feat(common): Import `DecompilerBase`

--- common/src/main/java/com/abiddarris/common/renpy/rpy/decompiler/atldecompiler.py
+++ common/src/main/java/com/abiddarris/common/renpy/rpy/decompiler/atldecompiler.py
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from .util import DecompilerBase, WordConcatenator, Dispatcher
+from .util import , WordConcatenator, Dispatcher
 
 import renpy
 
