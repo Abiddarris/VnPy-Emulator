837174106ec884c07177ffc5cf2ba5b1cff39dce
Abiddaris <abiddaris1101@gmail.com>
2025-04-18 21:24:12 +0800

    feat(app): Prepare for `0.3.5`

--- app/src/main/java/com/abiddarris/vnpyemulator/sources/Source.java
+++ app/src/main/java/com/abiddarris/vnpyemulator/sources/Source.java
@@ -29,7 +29,7 @@ import java.util.List;
  */
 public abstract class Source {
 
-    public static final String VERSION = "0.3.4";
+    public static final String VERSION = "0.3.5";
 
     /**
      * For testing purpose, {@link #getSource()} will provide
