74949b47a2b06b277a50e2b0ab085a67e634afaf
Abiddaris <abiddaris1101@gmail.com>
2025-04-22 21:06:45 +0800

    build(app): Prepare for `0.3.6-beta`

--- app/src/main/java/com/abiddarris/vnpyemulator/sources/Source.java
+++ app/src/main/java/com/abiddarris/vnpyemulator/sources/Source.java
@@ -29,7 +29,7 @@ import java.util.List;
  */
 public abstract class Source {
 
-    public static final String VERSION = "0.3.5";
+    public static final String VERSION = "0.3.6";
 
     /**
      * For testing purpose, {@link #getSource()} will provide
